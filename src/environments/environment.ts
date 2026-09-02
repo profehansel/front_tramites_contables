53ce3576beb1b501434d0aeba8d67013047a31a2
hanselxavier <hanseltzaj75@gmail.com>
2026-09-02 11:17:08 -0600

    primero

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'apitramitescontables-production.up.railway.app/api',
+  apiUrl: 'https://apitramitescontables-production.up.railway.app/api',
 };
